f76e7305eaede3ca128bb789c99ea5116098f212
Eduardo Ramos <eduardo@freedominterface.org>
2015-08-01 00:46:52 -0300

    Created index and new actions.

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -1,6 +1,25 @@
 class DomainsController < ApplicationController
 
   def index
+    @domains = Domain.all
   end
 
+  def new
+    @domain = Domain.new()
+  end
+
+  def create
+    @domain = Domain.new(domain_params)
+
+    if @domain.save
+      redirect_to domains_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def domain_params
+      params.require(:domain).permit(:name, :description)
+    end
 end
--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,2 +1,3 @@
 class Domain < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
 end
