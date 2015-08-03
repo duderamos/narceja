d90d8d5da8269663fd4eb63f2f08c8c25aa6d257
duderamos <eduardo@freedominterface.org>
2015-08-03 12:03:35 -0300

    Complete Domain entity.

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -8,6 +8,20 @@ class DomainsController < ApplicationController
     @domain = Domain.new()
   end
 
+  def edit
+    @domain = Domain.find(params[:id])
+  end
+
+  def update
+    @domain = Domain.find(params[:id])
+
+    if @domain.update(domain_params)
+      redirect_to domains_path
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @domain = Domain.new(domain_params)
 
@@ -16,6 +30,13 @@ class DomainsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def destroy
+    @domain = Domain.find(params[:id])
+    @domain.destroy
+
+    redirect_to domains_path
   end
 
   private
--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,3 +1,4 @@
 class Domain < ActiveRecord::Base
+  has_many :mailbox, :dependent => :delete_all
   validates :name, presence: true, uniqueness: true
 end
