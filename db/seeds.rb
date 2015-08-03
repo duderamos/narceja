1cdc11ed93fc57b669d7c5d6de1301320aa2ffc9
duderamos <eduardo@freedominterface.org>
2015-08-03 14:42:54 -0300

    seed.rb for initial data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
 Format.create([{ name: 'maildir' }, { name: 'sdbox' }])
+Domain.create([{ name: 'example', description: 'Just an example' }])
+User.new({ email: 'postmaster@example.com', password: 'a1b2c3d4', password_confirmation: 'a1b2c3d4'}).save
